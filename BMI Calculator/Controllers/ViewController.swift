3e803d5b7316ef2c00ba3742c49d7a307571ccae
matthiasvanwoensel <matthias@fluvio.be>
2022-06-23 16:55:38 +0200

    bmi word berekend

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -12,18 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
-    @IBAction func heightSlider(_ sender: UISlider) {
-        
-        let height = String(format: "%.2f",sender.value)
-        heightLabel.text = "\(height)m"
-    }
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     
-    @IBAction func weightSlider(_ sender: UISlider) {
-        
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f",sender.value)
+        heightLabel.text = "\(height)m"    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
         let weight = String(format: "%.0f",sender.value)
-        
         weightLabel.text = "\(weight)Kg"
-        
     }
     
     override func viewDidLoad() {
@@ -31,6 +30,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = String(format: "%.2f",weight / pow(height,2))
+        
+        print(bmi)
+    }
+    
 }
 
